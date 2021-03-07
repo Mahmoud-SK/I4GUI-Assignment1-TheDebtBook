e1a9c00e07d38a7921d84d75cbfde4e4073bdee3
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-03-07 15:14:25 +0100

    Klasser tilføjet til tomme mapper

---
+++ I4GUI-Assignment1-TheDebtBook/ViewModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4GUI_Assignment1_TheDebtBook.ViewModel
+{
+	class Class1
+	{
+	}
+}
