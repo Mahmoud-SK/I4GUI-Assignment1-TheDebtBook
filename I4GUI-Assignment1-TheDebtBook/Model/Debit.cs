9b74b361728d018cdf572e26fb70953728e10ec3
Lasseerenged <201806450@post.au.dk>
2021-03-07 15:28:41 +0100

    Debit model v1

---
+++ I4GUI-Assignment1-TheDebtBook/Model/Debit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4GUI_Assignment1_TheDebtBook.Model
+{
+    public class Debit
+    {
+        private int amount;
+        private string date;
+
+        public Debit()
+        {
+        }
+
+        public Debit(int dAmount, string dDate)
+        {
+            amount = dAmount;
+            date = dDate;
+        }
+
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+            }
+        }
+
+    }
+}
