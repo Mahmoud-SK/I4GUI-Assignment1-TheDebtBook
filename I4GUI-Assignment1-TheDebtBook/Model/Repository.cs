7264f4320e431264c74054a3d143a2ad9ce7a175
Magn2138 <magnusgk2000@gmail.com>
2021-03-08 09:37:00 +0100

    added ViewModel stuff

---
+++ I4GUI-Assignment1-TheDebtBook/Model/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace I4GUI_Assignment1_TheDebtBook
+{
+	public class Repository
+	{
+		internal static bool ReadFile(string fileName, out ObservableCollection<Debtor> debtor)
+		{
+			// Create an instance of the XmlSerializer class and specify the type of object to deserialize.
+			XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Debtor>));
+
+			TextReader reader = new StreamReader(fileName);
+			// Deserialize all the agents.
+			debtor = (ObservableCollection<Debtor>)serializer.Deserialize(reader);
+			reader.Close();
+
+			return true;
+		}
+
+		internal static void SaveFile(string fileName, ObservableCollection<Debtor> debtor)
+		{
+			// Create an instance of the XmlSerializer class and specify the type of object to serialize.
+			XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Debtor>));
+			TextWriter writer = new StreamWriter(fileName);
+			// Serialize all the agents.
+			serializer.Serialize(writer, debtor);
+			writer.Close();
+		}
+	}
+}
